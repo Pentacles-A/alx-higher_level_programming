98cee1f9f68c70953cb588c0395cca9e814303a3
Pentacles-A <adewaleabdulgafar@gmail.com>
2022-07-05 21:38:38 +0100

    Python input and output

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module for p_tri"""
+
+
+def pascal_triangle(n):
+    """to make this tri"""
+    tri = []
+    line = [1]
+    y = [0]
+    for x in range(max(n, 0)):
+        tri.append(line)
+        line = [l + r for l, r in zip(line + y, y + line)]
+    return tri
